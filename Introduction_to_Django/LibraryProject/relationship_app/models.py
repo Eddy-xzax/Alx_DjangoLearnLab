86b6cd0058bad3e4458a3e9e8250449c0cda0cae
Kiyingi Edrine <kiyinjieddrin@gmail.com>
2025-07-21 23:11:57 +0000

    Update

--- Introduction_to_Django/LibraryProject/relationship_app/models.py
+++ Introduction_to_Django/LibraryProject/relationship_app/models.py
@@ -1,23 +1,22 @@
 from django.db import models
 
 # Create your models here.
-class Author(models.Models):
+class Author(models.Model):
     name = models.CharField(max_length=100)
-
     def __str__(self):
         return self.name
 
-class Book(models.Models):
+class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         return self.Book
-class Library(models.Models):
+class Library(models.Model):
     name = models.CharField(max_length=50)
     books = models.ManyToManyField(Book)
     def __str__(self):
         return self.Library
-class Libralian(models.Models):
+class Libralian(models.Model):
     name = models.CharField(max_length=50)
     library = models.OneToManyField(Library)
     def __str__(self):
