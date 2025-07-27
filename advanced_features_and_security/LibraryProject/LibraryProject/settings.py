f8786377da0a7f7961c5286539be0e9cc794094f
Eddy-xzax <kiyinjieddrin@gmail.com>
2025-07-27 23:47:23 +0200

    Updated

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -131,10 +131,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL = '/'  # Where to go after login
-LOGOUT_REDIRECT_URL = '/login/'  # Where to go after logout
 
 AUTH_USER_MODEL = 'bookshelf.CustomUser'
+
 LOGIN_URL = 'bookshelf:login'
 LOGIN_REDIRECT_URL = 'bookshelf:list_books'
 LOGOUT_REDIRECT_URL = 'bookshelf:login'
+
