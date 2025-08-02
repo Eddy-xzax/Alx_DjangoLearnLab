dea169cddff6a4521d7d448010238706f74f02f3
Eddy-xzax <kiyinjieddrin@gmail.com>
2025-08-02 23:53:31 +0200

    Updated

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,5 @@
-from django.urls import path, include
+from django.urls import path
 from .views import BookList
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')), 
 ]
