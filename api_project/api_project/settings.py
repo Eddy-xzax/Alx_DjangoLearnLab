0d65d0561e87b8c406c4a4af0c4ce1f6fa849c44
Eddy-xzax <kiyinjieddrin@gmail.com>
2025-08-03 00:56:52 +0200

    Updated

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -54,7 +54,10 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES' : [
         'rest_framework.authentication.TokenAuthentication',
-     ]
+     ],
+    'DEFAULT_PERMISSION_CLASSES' : [
+        'rest_framework.permissions.IsAuthenticated',
+     ],
 }
 
 ROOT_URLCONF = 'api_project.urls'
