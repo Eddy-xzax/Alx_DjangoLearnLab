c93af6599b19e69a12f29c2c86ed9927e1f0a9d8
Eddy-xzax <kiyinjieddrin@gmail.com>
2025-08-19 17:22:33 +0200

    updated

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -3,7 +3,40 @@ from .forms import CustomUserCreationForm
 from .forms import UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 from .models import UserProfile
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
+from .models import Post
+
+# List all posts
+class PostListView(ListView):
+    model = Post
+    template_name = "post_list.html"
+    context_object_name = "posts"
+
+# View a single post
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
+
+# Create a new post
+class PostCreateView(CreateView):
+    model = Post
+    template_name = "post_form.html"
+    fields = ["title", "content"]
+
+# Update a post
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = "post_form.html"
+    fields = ["title", "content"]
+
+# Delete a post
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = "post_confirm_delete.html"
+    success_url = reverse_lazy("post_list")
 
 def register(request):
     form = CustomUserCreationForm()
