e4ae22eaef856b501a22c8ac57ab58129ac9950d
Eddy-xzax <kiyinjieddrin@gmail.com>
2025-08-19 17:25:37 +0200

    updated

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
+    path("", PostListView.as_view(), name="post_list"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
+    path("post/new/", PostCreateView.as_view(), name="post_new"),
+    path("post/<int:pk>/edit/", PostUpdateView.as_view(), name="post_edit"),
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post_delete"),
     path('login/', LoginViews.as_view(template_name='blog/login.html'), name='login'),
     path('register/', views.register, name='register'),
     path('logout/', LogoutViews.as_view(next_page='login'), name='logout'),
