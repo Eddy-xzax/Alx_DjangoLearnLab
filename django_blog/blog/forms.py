d8efd5f25b3d5f4b8b0c58b8d577c149fbc1b5c8
Eddy-xzax <kiyinjieddrin@gmail.com>
2025-08-20 23:43:15 +0200

    updated

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,7 @@
 # blog/forms.py
 from django import forms
-from .models import Comment
+from .models import Comment, Post
+from taggit.forms import TagWidget
 
 class CommentForm(forms.ModelForm):
     class Meta:
@@ -12,4 +13,14 @@ class CommentForm(forms.ModelForm):
                 "rows": 3,
                 "placeholder": "Write your comment here..."
             }),
+        }
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ["title", "content", "tags"]   # now includes tags
+        widgets = {
+            "title": forms.TextInput(attrs={"class": "form-control"}),
+            "content": forms.Textarea(attrs={"class": "form-control", "rows": 5}),
+            "tags": TagWidget(attrs={"class": "form-control"}),  # tag input
         }
