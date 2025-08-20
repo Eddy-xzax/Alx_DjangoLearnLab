1552b7238923b12fe8afd377149014238a4b36d5
Eddy-xzax <kiyinjieddrin@gmail.com>
2025-08-20 22:43:20 +0200

    updated

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -16,3 +16,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)  # when comment created
+    updated_at = models.DateTimeField(auto_now=True)      # when comment updated
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post}"
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -6,7 +6,6 @@ from .models import UserProfile
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-
 from .models import Post
 
 # List all posts
@@ -15,12 +14,10 @@ class PostListView(ListView):
     template_name = "post_list.html"
     context_object_name = "posts"
 
-# View a single post
 class PostDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
 
-# Create a new post
 class PostCreateView(CreateView):
     model = Post
     template_name = "post_form.html"
