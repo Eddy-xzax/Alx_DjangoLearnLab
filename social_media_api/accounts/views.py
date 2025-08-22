ab0c0a4c891ee9bb0aa602f02eb6ca96224c1cfa
Eddy-xzax <kiyinjieddrin@gmail.com>
2025-08-23 00:00:10 +0200

    updated

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from .models import CustomUser
+from .serializers import UserSerializer
 
-# Create your views here.
+
+# Example: List all users (only for authenticated users)
+class UserListView(generics.GenericAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        users = CustomUser.objects.all()
+        serializer = self.get_serializer(users, many=True)
+        return Response(serializer.data)
