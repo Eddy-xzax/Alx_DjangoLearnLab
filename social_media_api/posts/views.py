6c844cb212a2d3994bcb17271a69fbb07c4f6647
Eddy-xzax <kiyinjieddrin@gmail.com>
2025-08-23 00:03:05 +0200

    updated

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -2,7 +2,25 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializer
 
+
+class FollowingPostsView(generics.GenericAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        # get all the users the current user follows
+        following_users = request.user.following.all()
+
+        # filter posts by those users and order them (latest first)
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
+
+        serializer = self.get_serializer(posts, many=True)
+        return Response(serializer.data)
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
