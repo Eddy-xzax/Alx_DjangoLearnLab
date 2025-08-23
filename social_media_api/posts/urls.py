6e2fdf2be6d8537de8b1bf2e854a067caa7969b3
Eddy-xzax <kiyinjieddrin@gmail.com>
2025-08-23 22:40:47 +0200

    updated

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -3,4 +3,6 @@ from .views import FollowingPostsView
 
 urlpatterns = [
     path("feed/", FollowingPostsView.as_view(), name="feed"),
+    path('<int:pk>/like/', views.LikePostView.as_view(), name='like-post'),
+    path('<int:pk>/unlike/', views.UnlikePostView.as_view(), name='unlike-post'),
 ]
